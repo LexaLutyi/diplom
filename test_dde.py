b899952b2ef6cb5e116d73986845fba2002b7847
LexaLutyi <liotbiu1@gmail.com>
2019-01-20 16:14:43 +0300

    Добавил графики и радиус

--- test_dde.py
+++ test_dde.py
@@ -30,12 +30,61 @@ t_delta = 0.01
 t_delay = 0.1
 # сетка значений времени
 tt = arange(t_start, t_end, t_delta)
+# радиус видимости агентов
+vision_radius = 1.5
 
 # начальные условия в системе выбираем случайными
 x0 = random.randn(n_agents * n_axis * n_dim)
 
-yy = ddeint(model_second_order_with_delay, lambda t: x0, tt, fargs=(10, t_delay, n_agents))
+x = ddeint(model_second_order_with_delay, lambda t: x0, tt, fargs=(vision_radius, t_delay, n_agents))
 
-plt.plot(tt, yy)
+# представляем решение в удобном виде
+x = x.reshape(tt.size, n_dim, n_axis, n_agents)
 
+plt.figure()
+# словарь названий осей
+label_dict = {
+    (0, 0): 'x',
+    (1, 0): 'x\'',
+    (0, 1): 'y',
+    (1, 1): 'y\'',
+    (0, 2): 'z',
+    (1, 2): 'z\''
+}
+
+for i_dim in range(n_dim):
+    for i_axis in range(n_axis):
+        # график агентов вдоль одной оси
+        plt.plot(tt[:], x[:, i_dim, i_axis, :])
+        plt.xlabel('t')
+        plt.ylabel(label_dict[(i_dim, i_axis)])
+        plt.show()
+
+# график агентов на фазовой плоскости
+"""
+plt.plot(x[:, 0, 0, :], x[:, 0, 1, :])
+plt.xlabel(label_dict[(0, 0)])
+plt.ylabel(label_dict[(0, 1)])
+plt.show()
+"""
+y = (sum(x[:, 1, :, :], axis=(2,)) / n_agents)
+plt.plot(tt, y)
+plt.xlabel('t')
+plt.ylabel('Средняя скорость')
+plt.legend(['x', 'y'])
+plt.show()
+
+y = y.reshape(tt.size, n_axis, 1)
+q = einsum('...k, ...k', x[:, 1, :, :] - y, x[:, 1, :, :] - y)
+plt.xlabel('t')
+plt.ylabel('Среднеквадратичное отклонение от средней скорости')
+plt.plot(tt, q)
+plt.legend(['x', 'y'])
+plt.show()
+
+q = einsum('...k, ...k', x[:, 1, :, 1:-1] - x[:, 1, :, 0:-2], x[:, 1, :, 1:-1] - x[:, 1, :, 0:-2]) / n_agents
+plt.xlabel('t')
+plt.ylabel('Изменение скорости')
+plt.plot(tt, q)
+plt.legend(['x', 'y'])
 plt.show()
