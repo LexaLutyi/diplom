db0bae4c6714e41f9262d65f311b69c47d5c24e6
LexaLutyi <liotbiu1@gmail.com>
2019-01-20 15:59:20 +0300

    Моделирование задержки v 0.0

--- test_dde.py
+++ test_dde.py
@@ -1,20 +1,40 @@
 
 """ DDE with parameters. """
-
+import matplotlib.pyplot as plt
+from scipy.integrate import odeint
+from numpy import *
 from pylab import *
 from ddeint import ddeint
+from my_model import model_second_order_with_delay
 
 
-def model(Y, t):
-    x1, x2 = Y(t-1)
-    x = array([-x1, -x2])
-    return x
+# фиксируем случайность
+# seed = random.randint(1, 100000000)
+print('seed =', seed)
 
+# число агентов
+n_agents = 10
+# размерность пространства
+n_axis = 2
+# порядок системы
+n_dim = 2
 
-tt = linspace(0, 30, 100)
-yy = ddeint(model, lambda t:array([1, 2]), tt)
+# время начала моделирования
+t_start = 0
+# время конца моделирования
+t_end = 5
+# шаг по времени
+t_delta = 0.01
+# время задержки
+t_delay = 0.1
+# сетка значений времени
+tt = arange(t_start, t_end, t_delta)
 
-fig, ax = subplots(1,figsize=(4,4))
-ax.plot(tt, yy)
+# начальные условия в системе выбираем случайными
+x0 = random.randn(n_agents * n_axis * n_dim)
+
+yy = ddeint(model_second_order_with_delay, lambda t: x0, tt)
+
+plot(tt, yy)
 
 show()
